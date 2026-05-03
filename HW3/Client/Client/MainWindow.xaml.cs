a4b7264bd8de6b3fa4774452ebe7f85aab91c496
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2026-05-03 17:54:28 +0300

    HW3

---
+++ HW3/Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        UdpClient client;
+        IPEndPoint server;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new UdpClient();
+        }
+
+        private void SendBtn(object sender, RoutedEventArgs e)
+        {
+            string ip = ipBox.Text;
+            int port = int.Parse(portBox.Text);
+
+            server = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            string text = msgBox.Text;
+
+            if (string.IsNullOrWhiteSpace(text))
+                return;
+
+            Send("MSG|" + text);
+            msgBox.Clear();
+
+            Listen();
+        }
+
+        private async void Listen()
+        {
+            var result = await client.ReceiveAsync();
+            string msg = Encoding.UTF8.GetString(result.Buffer);
+
+            if (msg.StartsWith("RESP|"))
+            {
+                string answer = msg.Substring(5);
+
+                Dispatcher.Invoke(() =>
+                {
+                    chatBox.Items.Add("Bot: " + answer + "   " + DateTime.Now.ToShortTimeString());
+                });
+            }
+        }
+
+        private void Send(string msg)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            client.Send(data, data.Length, server);
+        }
+    }
+}
---
+++ HW3/Server/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static List<string> answers = new List<string>()
+    {
+        "Fine!",
+        "Hello!",
+        "I'm listening...",
+        "Interesting...",
+        "Could you explain more?",
+        "OK 👍",
+        "I don't understand, but sounds good"
+    };
+
+    static void Main()
+    {
+        UdpClient server = new UdpClient(8080);
+        IPEndPoint client = new IPEndPoint(IPAddress.Any, 0);
+
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            byte[] data = server.Receive(ref client);
+            string msg = Encoding.UTF8.GetString(data);
+
+            Console.WriteLine("Received: " + msg);
+
+            string response = GenerateResponse(msg);
+
+            byte[] outData = Encoding.UTF8.GetBytes("RESP|" + response);
+
+            server.Send(outData, outData.Length, client);
+        }
+    }
+
+    static string GenerateResponse(string msg)
+    {
+        msg = msg.ToLower();
+
+        if (msg.Contains("hello"))
+            return "Hello 👋";
+
+        if (msg.Contains("how"))
+            return "I'm fine, thanks!";
+
+        if (msg.Contains("name"))
+            return "I'm UDP Bot 🤖";
+
+        // fallback random
+        Random rnd = new Random();
+        return answers[rnd.Next(answers.Count)];
+    }
+}
