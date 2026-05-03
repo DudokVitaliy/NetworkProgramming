3f085a55e6bb6f66fede67801f4d31def74524ff
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2026-05-03 18:27:09 +0300

    HW6

---
+++ HW6/Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Win32;
+using System.Windows;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        string filePath = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AttachBtn(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            if (dlg.ShowDialog() == true)
+            {
+                filePath = dlg.FileName;
+                fileLabel.Text = filePath;
+            }
+        }
+
+        private void SendBtn(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress(emailBox.Text);
+                mail.To.Add(toBox.Text);
+                mail.Subject = subjectBox.Text;
+                mail.Body = bodyBox.Text;
+
+                if (!string.IsNullOrEmpty(filePath))
+                {
+                    mail.Attachments.Add(new Attachment(filePath));
+                }
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.Credentials = new NetworkCredential(emailBox.Text, passBox.Password);
+                smtp.EnableSsl = true;
+
+                smtp.Send(mail);
+
+                MessageBox.Show("Email sent!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
