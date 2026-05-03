c72c9a6e1ebdb9fb0e2459e257d8cb41e5a838e7
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2026-05-03 17:59:55 +0300

    HW4

---
+++ HW4/Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        TcpClient client;
+        NetworkStream stream;
+        string name;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectBtn(object sender, RoutedEventArgs e)
+        {
+            name = nameBox.Text;
+
+            client = new TcpClient("127.0.0.1", 5000);
+            stream = client.GetStream();
+
+            Send("JOIN|" + name);
+
+            Thread t = new Thread(Listen);
+            t.Start();
+        }
+
+        private void SendBtn(object sender, RoutedEventArgs e)
+        {
+            string text = msgBox.Text;
+
+            if (string.IsNullOrWhiteSpace(text)) return;
+
+            string time = DateTime.Now.ToShortTimeString();
+
+            Send($"MSG|{name}|{text}|{time}");
+
+            msgBox.Clear();
+        }
+
+        private void Listen()
+        {
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                try
+                {
+                    int size = stream.Read(buffer, 0, buffer.Length);
+                    string msg = Encoding.UTF8.GetString(buffer, 0, size);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        chatBox.Items.Add(msg);
+                    });
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Send(string msg)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            stream.Write(data, 0, data.Length);
+        }
+    }
+}
---
+++ HW4/Server/Server/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static TcpListener server;
+    static List<TcpClient> clients = new List<TcpClient>();
+
+    static void Main()
+    {
+        server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            clients.Add(client);
+
+            Console.WriteLine("Client connected");
+
+            Thread t = new Thread(HandleClient);
+            t.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            try
+            {
+                int size = stream.Read(buffer, 0, buffer.Length);
+                if (size == 0) break;
+
+                string msg = Encoding.UTF8.GetString(buffer, 0, size);
+
+                Console.WriteLine("Received: " + msg);
+
+                Broadcast(msg);
+            }
+            catch
+            {
+                break;
+            }
+        }
+
+        clients.Remove(client);
+        client.Close();
+    }
+
+    static void Broadcast(string message)
+    {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+
+        foreach (var client in clients)
+        {
+            try
+            {
+                client.GetStream().Write(data, 0, data.Length);
+            }
+            catch { }
+        }
+    }
+}
